400a4b47d42021276accaa596d0cb979043e9e7c
Josip-github <josipcota1@gmail.com>
2022-11-26 12:54:46 +0100

    Create components folder inside of src. Refer to the LearningResource.vue in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-    <ul></ul>
+    <ul>
+        <learning-resource v-for="res in storedResources" :key="res.id"></learning-resource>
+    </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
 export default {
+    components: {
+        LearningResource
+    },
     data() {
         return {
             storedResources: [
